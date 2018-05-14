65006221bd5d5c3dde739b2f86b8a8fd80b4f985
Keigo Mastuo <m.keigo.sp.ku@gmail.com>
2018-05-15 01:27:06 +0900

    add

---
+++ hotel_analysis.R
@@ -0,0 +1,6 @@
+
+#retrive chain hotel data
+osaka <- read.csv("https://raw.githubusercontent.com/kmbsweb/hotel-location/master/osakach.csv")
+tokyo <- read.csv("https://raw.githubusercontent.com/kmbsweb/hotel-location/master/tokyoch.csv")
+
+#and then...
