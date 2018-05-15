2419f3249dee1aca4689270e1c50036a50791172
Keigo Mastuo <m.keigo.sp.ku@gmail.com>
2018-05-15 10:22:12 +0900

    add

--- hotel_analysis.R
+++ hotel_analysis.R
@@ -4,3 +4,15 @@ osaka <- read.csv("https://raw.githubusercontent.com/kmbsweb/hotel-location/mast
 tokyo <- read.csv("https://raw.githubusercontent.com/kmbsweb/hotel-location/master/tokyoch.csv")
 
 #and then...
+library(Jtransit)
+library(dplyr)
+
+time    <- tokyo$narita_dur
+or <- tokyo$hotel_name
+dataF <- data.frame(origin=or,time=time)
+
+# result
+DD <- covert_m(dataF,2)
+tokyo$narita_dur <- DD$Time.sum
+
+write.csv(tokyo,"chtokyo.csv")
